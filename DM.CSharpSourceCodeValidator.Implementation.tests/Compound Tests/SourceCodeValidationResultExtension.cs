4efe74ce698fe4efbbf6d5ff47c00f692c30340c
Martin Edwin Schjødt Nielsen <MESN@ucl.dk>
2021-09-05 20:58:41 +0200

    Compound tests added for SourceCodeValiationResultExtention

--- DM.CSharpSourceCodeValidator.Implementation.tests/Compound Tests/SourceCodeValidationResultExtension.cs
+++ DM.CSharpSourceCodeValidator.Implementation.tests/Compound Tests/SourceCodeValidationResultExtension.cs
@@ -1,9 +1,8 @@
 ﻿using DM.CSharpSourceCodeValidator.Contracts;
 using DM.CSharpSourceCodeValidator.Models;
 using DM.CSharpSourceCodeValidator.Models.Extensions;
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,10 +14,8 @@ namespace DM.CSharpSourceCodeValidator.Implementation.tests.Compound_Tests
             :base(output){}
 
         [Fact]
-        public void GetNumberOfLinesWithError_ValidateTheCorrectNumberOfErrorsLineisProvide_IntValue2IsReturned()
+        public void GetNumberOfLinesWithError_ValidateTheCorrectNumberOfErrorsLineisProvideWithInvalidSourceCode_IntValue2IsReturned()
         {
-            
-
             SourceCodeValidator sut = GetSourceCodeValidator();
             string sourceCode = getInvalidSourceCodeWithSingleErrorLineAsString();
 
@@ -26,6 +23,39 @@ namespace DM.CSharpSourceCodeValidator.Implementation.tests.Compound_Tests
 
             Assert.Equal(2, result.GetNumberOfLinesWithError());
 
+        }
+        [Fact]
+        public void GetNumberOfLinesWithError_ValidateTheCorrectNumberOfErrorsLineisProvideWithValidSourceCode_IntValue0IsReturned()
+        {
+            SourceCodeValidator sut = GetSourceCodeValidator();
+            string sourceCode = getValidSourceCodeAsString();
+
+            SourceCodeValidationResult result = sut.ValidateSourceCode(sourceCode, "testAssembly");
+
+            Assert.Equal(0, result.GetNumberOfLinesWithError());
+
+        }
+        [Fact]
+        public void GetLineNumberForLinesWithErrors_ValidateThatAllErrorLineNumbersAreCorrectWithInvalidSourceCode_Returns8And9() {
+            SourceCodeValidator sut = GetSourceCodeValidator();
+            string sourceCode = getInvalidSourceCodeWithSingleErrorLineAsString();
+            IEnumerable<int> expected = new List<int> { 
+                9,10
+            };
+
+            SourceCodeValidationResult result = sut.ValidateSourceCode(sourceCode, "testAssembly");
+
+            Assert.Equal(expected, result.GetLineNumberForLinesWithErrors());
+        }
+        [Fact]
+        public void GetLineNumberForLinesWithErrors_ValidateThatNoErrorLinesAreReturnedWithValidSourceCode_ReturnEmptyCollection()
+        {
+            SourceCodeValidator sut = GetSourceCodeValidator();
+            string sourceCode = getValidSourceCodeAsString();
+
+            SourceCodeValidationResult result = sut.ValidateSourceCode(sourceCode, "testAssembly");
+
+            Assert.Empty(result.GetLineNumberForLinesWithErrors());
         }
     }
 }
