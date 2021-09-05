69b33dab9d09a796e998519416f6f5415a25e0ac
Martin Edwin Schjødt Nielsen <MESN@ucl.dk>
2021-09-05 20:16:21 +0200

    GetLineNumberForLinesWIthErrors , GetErrorMessages, Get ErrorMessageAndLineNumber extention methods added. (Untested)

--- DM.CSharpSourceCodeValidator.Models/Extensions/SourceCodeValidationResultExtensions.cs
+++ DM.CSharpSourceCodeValidator.Models/Extensions/SourceCodeValidationResultExtensions.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DM.CSharpSourceCodeValidator.Models.Extensions
@@ -7,9 +8,23 @@ namespace DM.CSharpSourceCodeValidator.Models.Extensions
     {
         public static int GetNumberOfLinesWithError(this SourceCodeValidationResult sourceCodeValidationResult)
             => sourceCodeValidationResult.ValidationErrors
-                    .Select(validationResult => validationResult.LineNumberBegin)
+                    .Select(validationError => validationError.LineNumberBegin)
                     .Distinct()
                     .Count();
+        public static IEnumerable<int> GetLineNumberForLinesWithErrors(this SourceCodeValidationResult sourceCodeValidationResult)
+             => sourceCodeValidationResult.ValidationErrors
+                    .Select(validationError => validationError.LineNumberBegin)
+                    .Distinct();
+        public static IEnumerable<string> GetErrorMessages(this SourceCodeValidationResult sourceCodeValidationResult)
+             => sourceCodeValidationResult.ValidationErrors
+                    .Select(validationError => validationError.ErrorMessage)
+                    .Distinct();
+        public static IEnumerable<KeyValuePair<int, string>> GetErrorMessageAndLineNumber(this SourceCodeValidationResult sourceCodeValidationResult)
+            => sourceCodeValidationResult.ValidationErrors
+                .Select(validationError=> 
+                        new KeyValuePair<int,string>(validationError.LineNumberBegin,validationError.ErrorMessage)
+                );
+
             
         
     }
